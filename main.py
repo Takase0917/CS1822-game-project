3d8b8f658439ffe53f1b7c57fa147d2a5a92eca9
Takase0917 <takase0917@icloud.com>
2022-02-23 12:37:00 +0000

    add background

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("this is main")
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+screen_width = 800
+screen_height = 700
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Platformer')
+
+background_img = pygame.image.load('img/background.jpg')
+
+run = True
+while run:
+    screen.blit(background_img, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
