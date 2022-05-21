34d16e38dfd347cbc4708e0e1059527db3de6fbf
Takase0917 <takase0917@icloud.com>
2022-05-21 10:29:35 +0100

    last push

--- SpacyFinal2.py
+++ SpacyFinal2.py
@@ -1,146 +1,9 @@
 try:
     import simplegui
     import random
-    import math
+    from vector import Vector
 except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
-
-# The Vector class
-
-
-class Vector:
-
-    # Initialiser
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
-
-    # Returns a string representation of the vector
-    def __str__(self):
-        return "(" + str(self.x) + "," + str(self.y) + ")"
-
-    # Tests the equality of this vector and another
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    # Tests the inequality of this vector and another
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    # Returns a tuple with the point corresponding to the vector
-    def get_p(self):
-        return (self.x, self.y)
-
-    def set_p(self, x, y):
-        self.x = x
-        self.y = y
-
-    # Returns a copy of the vector
-    def copy(self):
-        return Vector(self.x, self.y)
-
-    # Adds another vector to this vector
-    def add(self, other):
-        self.x += other.x
-        self.y += other.y
-        return self
-
-    def __add__(self, other):
-        return self.copy().add(other)
-
-    # Negates the vector (makes it point in the opposite direction)
-    def negate(self):
-        return self.multiply(-1)
-
-    def __neg__(self):
-        return self.copy().negate()
-
-    # Subtracts another vector from this vector
-    def subtract(self, other):
-        return self.add(-other)
-
-    def __sub__(self, other):
-        return self.copy().subtract(other)
-
-    # Multiplies the vector by a scalar
-    def multiply(self, k):
-        self.x *= k
-        self.y *= k
-        return self
-
-    def __mul__(self, k):
-        return self.copy().multiply(k)
-
-    def __rmul__(self, k):
-        return self.copy().multiply(k)
-
-    # Divides the vector by a scalar
-    def divide(self, k):
-        return self.multiply(1/k)
-
-    def __truediv__(self, k):
-        return self.copy().divide(k)
-
-    # Normalizes the vector
-    def normalize(self):
-        return self.divide(self.length())
-
-    # Returns a normalized version of the vector
-    def get_normalized(self):
-        return self.copy().normalize()
-
-    # Returns the dot product of this vector with another one
-    def dot(self, other):
-        return self.x * other.x + self.y * other.y
-
-    # Returns the length of the vector
-    def length(self):
-        return math.sqrt(self.x**2 + self.y**2)
-
-    # Returns the squared length of the vector
-    def length_squared(self):
-        return self.x**2 + self.y**2
-
-    # Reflect this vector on a normal
-    def reflect(self, normal):
-        n = normal.copy()
-        n.multiply(2*self.dot(normal))
-        self.subtract(n)
-        return self
-
-    # Returns the angle between this vector and another one
-    def angle(self, other):
-        return math.acos(self.dot(other) / (self.length() * other.length()))
-
-    # Rotates the vector 90 degrees anticlockwise
-    def rotate_anti(self):
-        self.x, self.y = -self.y, self.x
-        return self
-
-    # Rotates the vector according to an angle theta given in radians
-    def rotate_rad(self, theta):
-        rx = self.x * math.cos(theta) - self.y * math.sin(theta)
-        ry = self.x * math.sin(theta) + self.y * math.cos(theta)
-        self.x, self.y = rx, ry
-        return self
-
-    # Rotates the vector according to an angle theta given in degrees
-    def rotate(self, theta):
-        theta_rad = theta / 180 * math.pi
-        return self.rotate_rad(theta_rad)
-
-    # project the vector onto a given vector
-    def get_proj(self, vec):
-        unit = vec.get_normalized()
-        return unit.multiply(self.dot(unit))
-
-    # returns x value of vector
-    def getX(self):
-        return int(self.x)
-
-    # returns y value of vector
-    def getY(self):
-        return int(self.y)
 
 
 WIDTH = 700
@@ -442,7 +305,6 @@ class Interaction:
                 score_value = self.score.score_val - 100
 
     # check wether the meteorite within the canvas
-
     def is_meteorite_on_canvas(self, meteorite):
         # off top
         # center of the meteorite + the meteorite's radius -> the meteorite's edge
@@ -518,7 +380,7 @@ score = Score((550, 50), 0)
 
 inter = Interaction(meteorite, spaceship, kbd, countdown, score)
 
-frame = simplegui.create_frame('Interactions', CANVAS_DIMS[0], CANVAS_DIMS[1])
+frame = simplegui.create_frame('Asteroids', CANVAS_DIMS[0], CANVAS_DIMS[1])
 frame.set_draw_handler(inter.draw_handler)
 frame.set_keydown_handler(kbd.keyDown)
 frame.set_keyup_handler(kbd.keyUp)
